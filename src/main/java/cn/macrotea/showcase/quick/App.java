1b30028bc398984397355369bf8dbeb269b066ab
macrotea <macrotea@qq.com>
2014-08-16 14:42:32 +0800

    add test

--- src/main/java/cn/macrotea/showcase/quick/App.java
+++ src/main/java/cn/macrotea/showcase/quick/App.java
@@ -1,13 +1,43 @@
 package cn.macrotea.showcase.quick;
 
+import org.sphx.api.SphinxClient;
+import org.sphx.api.SphinxException;
+import org.sphx.api.SphinxMatch;
+import org.sphx.api.SphinxResult;
+
 /**
  * @author macrotea@qq.com
  * @since 2014-8-5 上午9:08
  */
 public class App {
 
-    public static void main(String[] args){
-        
+    // NOTICE macrotea@qq.com 2014-08-16 14:27:36 代码运行前配置好环境
+    // 更多的环境配置视频请访问: http://macrotea.com/tevi
+    //coolseek
+    //github 搜索 SphinxClient
+    public static void main(String[] args) throws SphinxException {
+        String host = "127.0.0.1";
+        int port = 9312;
+        int mode = SphinxClient.SPH_MATCH_ANY;
+
+        //构建客户端
+        SphinxClient client = new SphinxClient();
+        client.SetServer(host, port);
+        client.SetMatchMode(mode);
+        //理解为分页
+        client.SetLimits(0, 10);
+        client.SetMaxQueryTime(3000);
+
+        //执行搜索，第一个参数是关键字，第二个参数是索引
+        SphinxResult result = client.Query("测试", "*");
+        SphinxMatch[] matches = result.matches;
+
+        for (SphinxMatch match : matches) {
+            System.out.println(match.docId);
+        }
+
+
+        //88 thanks
     }
-    
+
 }
---
+++ src/main/java/org/sphx/api/SphinxMatch.java
@@ -0,0 +1,33 @@
+/*
+ * $Id: SphinxMatch.java 1172 2008-02-24 13:50:48Z shodan $
+ */
+
+package org.sphx.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Matched document information, as in search result.
+ */
+public class SphinxMatch {
+	/** Matched document ID. */
+	public long docId;
+
+	/** Matched document weight. */
+	public int weight;
+
+	/** Matched document attribute values. */
+	public List attrValues;
+
+	/** Trivial constructor. */
+	public SphinxMatch(long docId, int weight) {
+		this.docId = docId;
+		this.weight = weight;
+		this.attrValues = new ArrayList();
+	}
+}
+
+/*
+ * $Id: SphinxMatch.java 1172 2008-02-24 13:50:48Z shodan $
+ */
